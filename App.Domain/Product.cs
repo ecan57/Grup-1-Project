7872bd91d1b85f86c0043a292d17bd77f2969c3c
Emine CAN <eminecan570@gmail.com>
2024-05-27 15:04:07 +0300

    [Feature]: Product class updated.
    Description and CreatedOn properties added the Product class.

--- App.Domain/Product.cs
+++ App.Domain/Product.cs
@@ -4,4 +4,7 @@ public class Product
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTimeOffset CreatedOn { get; set; }
+
 }
